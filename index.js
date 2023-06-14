910259723b372b94e0aede0ec8a7c1b742b9ca7d
JOY19ANYANGO <joyanyango402@gmail.com>
2023-06-14 18:55:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  let result;
+  if (feet <= 400) {
+    result = 'This one is on me!';
+  } else if (feet >= 2000 && feet <= 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    result = 'No can do.';
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+scuberGreetingForFeet(199)
+scuberGreetingForFeet(2001)
+scuberGreetingForFeet(2501) 
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result
+  if(city==='NYC'){
+    result='Ok, sounds good.'
+  }else if(city==='Pittsburgh'){
+    result='No go.'
+  }
+  return result;
+}
+ternaryCheckCity('NYC')
+ternaryCheckCity('Pittsburgh')
+
+function switchOnCharmFromTip(tip){
+  let result;
+  switch(tip){
+    case 'generous':
+      result='Thank you so much.'
+      break
+    case 'not as generous':
+      result='Thank you.'
+       break
+     default:
+      result="Bye."        
+       }
+       return result
+
 }
